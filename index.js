3244b9ebd6ed77bc90dd0d5c4dcc0098251922c8
nvduy0511 <nvduy.k19hutech@gmail.com>
2022-05-23 21:22:44 +0700

    xong cpp-c

---
+++ index.js
@@ -0,0 +1,135 @@
+
+const express = require('express')
+const app = express()
+var path = require('path');
+
+app.use(express.json())
+app.use(express.urlencoded({
+  extended:true
+}));
+
+const port = 3002
+
+var compiler = require('compilex');
+var option = {stats : true};
+compiler.init(option);
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/delete', (req, res) => {
+  compiler.flush(function(){
+    console.log('All temporary files flushed !'); 
+    });
+  res.send('Delete Success!')
+})
+
+app.post('/a', function(req,res){
+  console.log(req.body.a)
+  res.send('aolk')
+})
+
+
+app.post('/compilecode' , function (req , res ) {
+
+	var code = req.body.code;	
+	var input = req.body.input;
+  var lang = req.body.language;
+  console.log(lang)
+  
+  if((lang === "c") || (lang === "cpp"))
+  {   
+    if(input === ""){
+        var envData = { OS : "windows" , cmd : "g++", options: {timeout:1000 }};	   
+          compiler.compileCPP(envData , code , function (data) {
+          if(data.error)
+          {
+            res.send(data.error);
+          }    	
+          else
+          {
+            res.send({output:data.output.replaceAll('\r','')});
+          }});
+    }     
+    else{    
+
+      var envData = { OS : "windows" , cmd : "g++", options: {timeout:1000 }};	   	
+      compiler.compileCPPWithInput(envData , code ,input , function (data) {
+      if(data.error)
+      {
+        res.send(data.error);    		
+      }
+      else
+      {
+        res.send({output:data.output.replaceAll('\r','')});
+      }});
+    }
+  
+  }
+  // if(lang === "Java")
+  // {
+  //     if(inputRadio === "true")
+  //     {
+  //         var envData = { OS : "windows" };     
+  //         console.log(code);
+  //         compiler.compileJavaWithInput( envData , code , function(data){
+  //             res.send(data);
+  //         });
+  //     }
+  //     else
+  //     {
+  //         var envData = { OS : "windows" };     
+  //         console.log(code);
+  //         compiler.compileJavaWithInput( envData , code , input ,  function(data){
+  //             res.send(data);
+  //         });
+
+  //     }
+
+  // }
+  // if( lang === "Python")
+  // {
+  //     if(inputRadio === "true")
+  //     {
+  //         var envData = { OS : "windows"};
+  //         compiler.compilePythonWithInput(envData , code , input , function(data){
+  //             res.send(data);
+  //         });            
+  //     }
+  //     else
+  //     {
+  //         var envData = { OS : "windows"};
+  //         compiler.compilePython(envData , code , function(data){
+  //             res.send(data);
+  //         });
+  //     }
+  // }
+  // if( lang === "CS")
+  // {
+  //     if(inputRadio === "true")
+  //     {
+  //         var envData = { OS : "windows"};
+  //         compiler.compileCSWithInput(envData , code , input , function(data){
+  //             res.send(data);
+  //         });            
+  //     }
+  //     else
+  //     {
+  //         var envData = { OS : "windows"};
+  //         compiler.compileCS(envData , code , function(data){
+  //             res.send(data);
+  //         });
+  //     }
+
+  // }
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+compiler.flush(function () {
+  console.log("All temporary files flushed !");
+});
